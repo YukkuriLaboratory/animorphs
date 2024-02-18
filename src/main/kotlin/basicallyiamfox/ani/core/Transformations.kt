07307de5c78afe66dffce7a346d74cd7b7ec278f
sysnote8main <sysnote8main@gmail.com>
2024-02-19 00:46:37 +0900

    :hammer: Add sting ability

--- src/main/kotlin/basicallyiamfox/ani/core/Transformations.kt
+++ src/main/kotlin/basicallyiamfox/ani/core/Transformations.kt
@@ -3,6 +3,7 @@ package basicallyiamfox.ani.core
 import basicallyiamfox.ani.extensions.ModIdentifier
 
 object Transformations {
+    @JvmField
     val BEE = ModIdentifier("bee")
     val MAGMA_CUBE = ModIdentifier("magma_cube")
     val NOTE_BLOCK = ModIdentifier("note_block")
